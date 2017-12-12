8c2264a27ad08aed71ce6a1b4895a6ea1e2ff33e
guillaume crouillere <gcrouillere@hotmail.fr>
2017-12-12 16:15:18 +0100

    admin photos displayed for everyone

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,16 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :retrieve_admin
   layout :layout_by_resource
   after_action :store_location
 
   def default_url_options
   { host: ENV["HOST"] || "localhost:3000" }
+  end
+
+  def retrieve_admin
+    @user = User.where(admin: true).first
   end
 
   #DEVISE methods:
