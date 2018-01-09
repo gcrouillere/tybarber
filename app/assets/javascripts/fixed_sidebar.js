a95e03d3c4a85176a87efb6266ab877d60fcf160
guillaume crouillere <gcrouillere@hotmail.fr>
2018-01-09 16:56:40 +0100

    changed hompage button

--- app/assets/javascripts/fixed_sidebar.js
+++ app/assets/javascripts/fixed_sidebar.js
@@ -20,3 +20,30 @@ $(document).ready(function() {
     changeButton();
   });
 })
+
+
+// Without jquery:
+
+// function changeButton() {
+//   if ( document.querySelector(".ui.builder_button.light-green") ) {
+//   var imageOffset = offset(document.querySelector("#image-3400-0-0-0"))
+//     if((imageOffset.top - document.querySelector("#header.header-on-scroll").offsetHeight) * (- 1) > document.querySelector("#image-3400-0-0-0").offsetHeight) {
+//       document.querySelector(".ui.builder_button.light-green").style.cssText = " position: fixed; top: 142px; left: calc(18%);"
+//     } else {
+//       document.querySelector(".ui.builder_button.light-green").style.cssText = " position: ''; top: ''; left: '';"
+//         };
+//       };
+//     };
+
+//   function offset(elt) {
+//     var rect = elt.getBoundingClientRect(), bodyElt = document.body;
+//     return {
+//         top: rect.top + bodyElt .scrollTop,
+//         left: rect.left + bodyElt .scrollLeft
+//     }
+//   }
+
+//   window.addEventListener('scroll', (event) => {
+//     changeButton();
+//   });
+
