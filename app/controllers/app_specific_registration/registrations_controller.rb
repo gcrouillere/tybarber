3f450d09c48a5827bc0c608562f3472b4a42a666
guillaume crouillere <gcrouillere@hotmail.fr>
2018-01-24 10:28:07 +0100

    changed provider user update

--- app/controllers/app_specific_registration/registrations_controller.rb
+++ app/controllers/app_specific_registration/registrations_controller.rb
@@ -1,5 +1,14 @@
 class AppSpecificRegistration::RegistrationsController < Devise::RegistrationsController
 
+  def update_resource(resource, params)
+    if current_user.provider == "facebook"
+      params.delete("password")
+      resource.update_without_password(params)
+    else
+      resource.update_with_password(params)
+    end
+  end
+
   protected
 
   def after_update_path_for(resource)
