f34a0df650d536e02468a0c612be1a7841b475ef
guillaume crouillere <gcrouillere@hotmail.fr>
2018-02-01 14:52:44 +0100

    step 1 ok

---
+++ db/migrate/20180201130513_create_themes.rb
@@ -0,0 +1,10 @@
+class CreateThemes < ActiveRecord::Migration[5.0]
+  def change
+    create_table :themes do |t|
+      t.string :name, null: false
+      t.boolean :active, null: false, default: false
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180111135404) do
+ActiveRecord::Schema.define(version: 20180201130513) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -147,6 +147,13 @@ ActiveRecord::Schema.define(version: 20180111135404) do
     t.integer  "port_cents",   default: 0, null: false
     t.index ["lesson_id"], name: "index_orders_on_lesson_id", using: :btree
     t.index ["user_id"], name: "index_orders_on_user_id", using: :btree
+  end
+
+  create_table "themes", force: :cascade do |t|
+    t.string   "name",                       null: false
+    t.boolean  "active",     default: false, null: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
   end
 
   create_table "users", force: :cascade do |t|
