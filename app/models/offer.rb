34c011e7a1244678a2e63087ff3c5f6af1555247
guillaume crouillere <gcrouillere@hotmail.fr>
2018-02-27 15:38:05 +0100

    changed locale erro messages

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -3,5 +3,5 @@ class Offer < ApplicationRecord
 
   validates :title, presence: true
   validates :description, presence: true
-  validates :discount, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 1, message:'Doit être compris entre 0 et 1 inclus' }}
+  validates :discount, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 1, message:'Doit être compris entre 0 et 1 inclus' }
 end
