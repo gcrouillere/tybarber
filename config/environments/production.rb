26d9bb69ccf9d64901bb35fd56c6369e0fe5a102
guillaume crouillere <gcrouillere@hotmail.fr>
2018-02-28 13:50:00 +0100

    removed poking

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,9 +97,9 @@ Rails.application.configure do
     :enable_starttls_auto => true
   }
 
-  Dynopoker.configure do |config|
-    config.address = 'https://carolekaltenbacher.herokuapp.com'
-    config.poke_frequency = 600 # default is 1800s (30min)
-  end
+  # Dynopoker.configure do |config|
+  #   config.address = 'https://carolekaltenbacher.herokuapp.com'
+  #   config.poke_frequency = 600 # default is 1800s (30min)
+  # end
 
 end
