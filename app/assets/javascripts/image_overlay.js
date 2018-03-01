b528409fc10dac678366f5ccd8c4b38254b48fd2
guillaume crouillere <gcrouillere@hotmail.fr>
2018-03-01 16:27:14 +0100

    add zoom info

--- app/assets/javascripts/image_overlay.js
+++ app/assets/javascripts/image_overlay.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
   if ($('.front-image-div').size() > 0) {
     $('.overlay-ok').on('click', function(e) {
       $('.imageoverlay').css("display", "none");
+      $('.img-front').css("filter", "brightness(100%)");
     });
   }
 });
