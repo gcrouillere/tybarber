a5051702f207d8e9c8eab192bf3cd5780daa853f
guillaume crouillere <gcrouillere@hotmail.fr>
2018-04-17 11:38:13 +0200

    change migration

--- db/migrate/20180416105437_add_takeaway_to_order.rb
+++ db/migrate/20180416105437_add_takeaway_to_order.rb
@@ -1,6 +1,6 @@
 class AddTakeawayToOrder < ActiveRecord::Migration[5.0]
   def change
-    add_column :orders, :take_away, :boolean, null: false
+    add_column :orders, :take_away, :boolean
     Order.update_all(take_away: false)
   end
 end
