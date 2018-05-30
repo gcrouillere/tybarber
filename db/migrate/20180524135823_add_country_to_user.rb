838e507174f4e87e1c5d5e3c816e81138d260811
guillaume crouillere <gcrouillere@hotmail.fr>
2018-05-30 08:59:57 +0200

    change root_url

--- db/migrate/20180524135823_add_country_to_user.rb
+++ db/migrate/20180524135823_add_country_to_user.rb
@@ -1,5 +1,5 @@
 class AddCountryToUser < ActiveRecord::Migration[5.0]
   def change
-    add_column :users, :country, :string, null: false, default: "A mettre à jour"
+    add_column :users, :country, :string, null: false, default: "FR"
   end
 end
