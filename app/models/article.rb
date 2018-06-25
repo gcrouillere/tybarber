b55272ba31bda08df3d0a9d4aeeee9c4017ede2a
guillaume crouillere <gcrouillere@hotmail.fr>
2018-06-25 10:08:29 +0200

    fix parametrize

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,7 +4,9 @@ class Article < ApplicationRecord
   validates :name, presence: :true
 
   has_attachment :article_main_photo
+
   def to_param
+    param_title = title.parameterize || ""
     [id, title.parameterize].join("-")
   end
 end
