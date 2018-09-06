f63b6d5a5f50e2d606d539b2c11d72ec82086ef1
guillaume crouillere <gcrouillere@hotmail.fr>
2018-09-06 13:26:15 +0200

    md card display

--- db/schema.rb
+++ db/schema.rb
@@ -15,13 +15,13 @@ ActiveRecord::Schema.define(version: 2018_05_29_071624) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "active_admin_comments", id: :serial, force: :cascade do |t|
+  create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
     t.text "body"
     t.string "resource_type"
-    t.integer "resource_id"
+    t.bigint "resource_id"
     t.string "author_type"
-    t.integer "author_id"
+    t.bigint "author_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id"
@@ -49,8 +49,8 @@ ActiveRecord::Schema.define(version: 2018_05_29_071624) do
     t.integer "height"
     t.string "format"
     t.string "resource_type"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["attachinariable_type", "attachinariable_id", "scope"], name: "by_scoped_parent"
   end
 
