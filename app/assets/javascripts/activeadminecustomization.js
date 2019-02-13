d068359fe5eb3e33a8686d5603f05fc966df88b8
guillaume crouillere <gcrouillere@hotmail.fr>
2019-02-13 22:53:46 +0100

    step 1 with js ok

--- app/assets/javascripts/activeadminecustomization.js
+++ app/assets/javascripts/activeadminecustomization.js
@@ -24,3 +24,59 @@ $(document).ready(function() {
     }
   });
 });
+
+$(document).ready(function() {
+  document.querySelectorAll('#index_table_produits tbody tr').forEach(
+    x => {
+      x.setAttribute('draggable', 'true');
+    }
+  );
+
+  var productRows = Array.from(document.querySelectorAll('#index_table_produits tbody tr'));
+
+  [].forEach.call(productRows, function(product_row) {
+    product_row.addEventListener('dragstart', onDragStart, false);
+    product_row.addEventListener('dragover', onDragOver, false);
+    product_row.addEventListener('drop', onDrop, false);
+  });
+});
+
+  var draggedProduct = null;
+  var draggedIndex = null;
+  var rowsInitialPositions = null
+  var productRows = null
+
+  onDragOver = (event) => {
+    if (event.preventDefault) {
+      event.preventDefault();
+    }
+  }
+
+  onDrop = (event) => {
+    if (event.stopPropagation) {
+      event.stopPropagation();
+    }
+
+    if (draggedProduct != event.srcElement.parentElement) {
+
+      let arrivalElement = event.srcElement.parentElement;
+      let arrivalIndex = rowsInitialPositions.indexOf(arrivalElement.querySelector('td.col-id').innerHTML);
+
+      productRows.splice(draggedIndex, 1);
+      productRows.splice(arrivalIndex, 0, draggedProduct);
+      let finalHTMLs = productRows.map(x => x.innerHTML);
+      Array.from(document.querySelectorAll('#index_table_produits tbody tr')).forEach((x, index) => {
+       x.innerHTML = finalHTMLs[index]
+      });
+
+    }
+    return false;
+  }
+
+  onDragStart = (event) => {
+    rowsInitialPositions = Array.from(document.querySelectorAll('#index_table_produits tbody tr td.col-id')).map(x => x.innerHTML);
+    productRows = Array.from(document.querySelectorAll('#index_table_produits tbody tr'));
+    draggedProduct = event.srcElement;
+    draggedIndex = rowsInitialPositions.indexOf(draggedProduct.querySelector('td.col-id').innerHTML);
+    event.dataTransfer.setData('text', draggedProduct);
+  }
