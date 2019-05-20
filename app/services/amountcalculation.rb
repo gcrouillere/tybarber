4fe17917496dd662199909c5c217dd85bd70fa38
guillaume crouillere <gcrouillere@hotmail.fr>
2019-05-20 17:55:28 +0200

    improved wiehgt over 30kg  shipping calc

--- app/services/amountcalculation.rb
+++ app/services/amountcalculation.rb
@@ -8,40 +8,30 @@ class Amountcalculation
   def calculate_amount(order, user)
     amount_ceramique = 0
     total_weight = 0
+    shipping_cost = 0
+    weight_partition = 0
+    remaining_cost = 0
+
     order.basketlines.each do |basketline|
       basketline.ceramique.offer ? ceramique_discount = basketline.ceramique.offer.discount : ceramique_discount = 0
       amount_ceramique += (basketline.ceramique.price * (1 - ceramique_discount)) * basketline.quantity
       total_weight += basketline.ceramique.weight * basketline.quantity
     end
-    if user
-      if user.country == "FR" && total_weight > 0
-        shipping_cost = ShippingCategory.where(alpha2: "FR").where("weight >= ?", total_weight).min.price_cents.to_f / 100
-        {total: amount_ceramique, port: shipping_cost.to_money, weight: total_weight}
-      elsif total_weight > 0
-        shipping_cost = ShippingCategory.where(alpha2: user.country).where("weight >= ?", total_weight).min.price_cents.to_f / 100
-        {total: amount_ceramique, port: shipping_cost.to_money, weight: total_weight}
-      else
-        {total: 0, port: 0, weight: 0}
-      end
-    else
-      if total_weight > 0
-        shipping_cost = ShippingCategory.where(alpha2: "FR").where("weight >= ?", total_weight).min.price_cents.to_f / 100
-        {total: amount_ceramique, port: shipping_cost.to_money, weight: total_weight}
-      else
-        {total: 0, port: 0, weight: 0}
-      end
-    end
-  end
 
-  # API Deprecated by La Poste
-  # def fr_shipping_cost(amount_ceramique, total_weight)
-  #   tarif_colis = HTTParty.get(
-  #     "https://api.laposte.fr/tarifenvoi/v1?type=colis&poids=#{total_weight}",
-  #     headers: {"X-Okapi-Key" => ENV['LAPOSTE_API_KEY'] }
-  #   )
-  #   tarif_colis.empty? ? port = ShippingCategory.where(alpha2: "FR").where("weight >= ?", total_weight).min.price_cents.to_f / 100 : port = tarif_colis[0]["price"]
-  #   return {total: amount_ceramique, port: port.to_money, weight: total_weight}
-  # end
+    weight_partition = { integerDivision: total_weight / 30000, remainingWeight: total_weight % 30000 }
+
+    if user
+      max_fare = ShippingCategory.where(alpha2: user.country).where("weight >= 30000", total_weight).max.price_cents.to_f / 100
+      remaining_cost = ShippingCategory.where(alpha2: user.country).where("weight >= ?", weight_partition[:remainingWeight]).min.price_cents.to_f / 100
+    else
+      max_fare = ShippingCategory.where(alpha2: "FR").where("weight >= 30000", total_weight).max.price_cents.to_f / 100
+      remaining_cost = ShippingCategory.where(alpha2: "FR").where("weight >= ?", weight_partition[:remainingWeight]).min.price_cents.to_f / 100
+    end
+
+    shipping_cost = max_fare * weight_partition[:integerDivision] + remaining_cost
+
+    return {total: amount_ceramique, port: shipping_cost.to_money, weight: total_weight}
+  end
 
 end
 
