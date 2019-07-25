5bbee74fb90a5b2818707eb731391591822087f6
guillaume crouillere <gcrouillere@gmail.com>
2019-07-25 09:38:51 +0200

    homepage visual changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -103,9 +103,9 @@ Rails.application.configure do
     :enable_starttls_auto => true
   }
 
-  Dynopoker.configure do |config|
-    config.address = 'https://tymorta.herokuapp.com'
-    config.poke_frequency = 600 # default is 1800s (30min)
-  end
+  # Dynopoker.configure do |config|
+  #   config.address = 'https://tymorta.herokuapp.com'
+  #   config.poke_frequency = 600 # default is 1800s (30min)
+  # end
 
 end
